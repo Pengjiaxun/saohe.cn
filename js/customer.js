8722b6ae96c86a251cfa5da19b643c6d2140cb36
彭佳勋 <420558221@qq.com>
2016-04-13 12:44:16 +0800

    官网2.0版本

--- js/customer.js
+++ js/customer.js
@@ -16,6 +16,26 @@
 	}
 });*/
 
+/*滚动显示二维码和回到顶部图标*/
+$(window).scroll(function () {
+	var t=$(document).scrollTop();
+	// if(t>500){
+	// 	$("#code1").attr('src','images/icon/gongzhonghao2.png');
+	// 	$("#code2").attr('src','images/icon/app_download2.png');
+	// }
+	// else{
+	// 	$("#code1").attr('src','images/icon/gongzhonghao.png');
+	// 	$("#code2").attr('src','images/icon/app_download.png');
+	// }
+	if(t>1000){
+		$(".btop").fadeIn('slow');
+	}
+	else{
+		$(".btop").fadeOut('slow');
+	}
+
+});
+
 /*导航颜色的改变*/
 $(".topr li a").mouseenter(function(){
 	$(this).css("color","#c84bdf");
@@ -24,41 +44,53 @@ $(".topr li a").mouseenter(function(){
 	$(this).css("color","white");
 });
 $(".topr li:first-child a").mouseenter(function(){
-	$(this).css("color","white");
+	$(this).addClass('mouseover');
+	$(this).css('color','#ffffff');
 })
 .mouseout(function(){
+	$(this).removeClass('mouseover');
+	$(this).css('color','#c84bdf');
+});
+
+$(".erweima a").mouseenter(function(){
 	$(this).css("color","#c84bdf");
+})
+.mouseout(function(){
+	$(this).css("color","white");
 });
 
 /*导航栏锚点平滑滚动*/
 $("#btn-introduce").click(function() {
 	$("html,body").animate({scrollTop:$("#page1").offset().top}, 500);
 	$(".topr li a").css("color","#ffffff");
-	$(".topr li:first-child a").css("color","#c84bdf");
+	// $(".topr li:first-child a").css("color","#c84bdf");
 });
 $("#btn-partner").click(function() {
 	$("html,body").animate({scrollTop:$("#partner").offset().top}, 500);
 	$(".topr li a").css("color","#ffffff");
-	$(".topr li:first-child a").css("color","#c84bdf");
+	// $(".topr li:first-child a").css("color","#c84bdf");
 	$(this).css("color","red");
 });
 $("#btn-manage").click(function() {
 	$("html,body").animate({scrollTop:$("#introduce").offset().top}, 500);
 	$(".topr li a").css("color","#ffffff");
-	$(".topr li:first-child a").css("color","#c84bdf");
+	// $(".topr li:first-child a").css("color","#c84bdf");
 	$(this).css("color","red");
 });
-$("#btn-echo").click(function() {
-	$("html,body").animate({scrollTop:$("#echo").offset().top}, 500);
+$("#btn-pro").click(function() {
+	$("html,body").animate({scrollTop:$("#ourpro").offset().top}, 500);
 	$(".topr li a").css("color","#ffffff");
-	$(".topr li:first-child a").css("color","#c84bdf");
+	// $(".topr li:first-child a").css("color","#c84bdf");
 	$(this).css("color","red");
+});
+$(".btop").click(function() {
+	$("html,body").animate({scrollTop:$("#top").offset().top}, 500);
 });
 
 /*二维码设置位置和显示*/
-var tdist=$("#page1").outerHeight(true)/3.5;
+/*var tdist=$("#page1").outerHeight(true)/3.5;
 $(".code-msg").css("top",tdist);
-$(".code-img").css("top",tdist+160);
+$(".code-img").css("top",tdist+160);*/
 $(".code-sh").mouseenter(function() {
 	$(this).parent().stop(true, false).animate({"right": 0}, 500);
 	// alert(tdist);
@@ -98,10 +130,10 @@ $(".img").mouseenter(function(){
 });
 
 /*选中图片浮起阴影效果*/
-$(".pic img,.echor img").mouseenter(function(){
+/*$(".pic img,.echor img").mouseenter(function(){
 	$(".pic img,.echor img").removeClass('imgselected');
 	$(this).addClass('imgselected');
 })
 .mouseout(function(){
 	$(this).removeClass('imgselected');
-});
+});*/
